9756cd35c47f4576295e743cbb4cb50b71ea5d1a
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-24 23:52:51 +0600

    object created

--- js/interactive.js
+++ js/interactive.js
@@ -1,6 +1,18 @@
-const books = {
-    type : ["Bangla", "English", "Math", "Social-science", "Science", "Religion"],
-    price: 120,
+class books  {
+    constructor(type, price, writer){
+        this.type = type;
+        this.price = price;
+        this.writer = writer;
+    }
 }
 
-const {book1}  = books;
+const bangla = new books("academic", 120, "Sunehra Chowdhury");
+const english = new books("academic", 80, "Suhaima Chowdhury");
+const math  = new books("academic", 120, "Ankur Chowdhury");
+const social = new books("academic", 230, "Suhaima Chowdhury");
+const science = new books("academic", 100, "Suhaima Chowdhury");
+const religion = new books("academic", 200, "Raisa Alam");
+
+console.log(bangla.type);
+console.log(religion.writer);
+
